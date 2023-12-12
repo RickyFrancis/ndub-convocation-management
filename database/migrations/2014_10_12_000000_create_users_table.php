065c93aa10a250f5696d01f0eaa48cb211f29e44
Md. Harun Aur Rashid Khan Ishan <ishanndub@gmail.com>
2023-12-12 15:13:22 +0600

    Testing phase

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,16 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+			$table->string('photo')->nullable();
+			$table->string('role_id')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('user_slug')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->string('deleted_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
